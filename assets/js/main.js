f6234455ad428c3d918023d19c03ff411d89117a
서도하 <seodoha@reflexion.co.kr>
2021-12-20 16:55:22 +0900

    33

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,8 +14,13 @@ import { Beverage, Water, Milk, Ice, Banilla, Caramel, Mocha } from "./beverage.
             $layer.classList.add("active");
         },
         close() {
+            const popHead = document.getElementsByClassName("popHead")[0],
+                  popChk = document.getElementById("hotChk");
+
             $dim.classList.remove("active");
             $layer.classList.remove("active");
+            popHead.innerText = '';
+            popChk.checked = true;
         },
     };
 
@@ -99,32 +104,68 @@ import { Beverage, Water, Milk, Ice, Banilla, Caramel, Mocha } from "./beverage.
             });
         },
         eventTrigger() {
-            const self = kioskApp;
+            const self = kioskApp,
+                  popHead = document.getElementsByClassName("popHead")[0],
+                  popChk = document.getElementById("hotChk");
 
-            console.log(new Beverage('espresso'));                          // 에스프레소
-            console.log(Water(new Beverage('americano')));                  // 아메리카노
-            console.log(Ice(Water(new Beverage('iceAmericano'))));          // 아이스 아메리카노
-            console.log(Milk(new Beverage('cafelatte')));                   // 카페라떼
-            console.log(Ice(Milk(new Beverage('iceCafelatte'))));           // 아이스 카페라떼
-            console.log(Banilla(Milk(new Beverage('banillalatte'))));       // 바닐라라떼
-            console.log(Ice(Banilla(Milk(new Beverage('iceBanillalatte')))));  // 아이스 바닐라라떼
-            console.log(Caramel(Milk(new Beverage('caramellatte'))));  // 카라멜라떼
-            console.log(Ice(Caramel(Milk(new Beverage('iceCaramelatte')))));  // 아이스 카라멜라떼
-            console.log(Mocha(Milk(new Beverage('cafemocha'))));  // 카페모카
-            console.log(Ice(Mocha(Milk(new Beverage('iceCafemocha')))));  // 아이스 카페모카
+            let tempEl = {},
+                setEl;
 
             coffeList.addEventListener('click', (e) => {
                 e.preventDefault();
                 const target = e.target || e.srcElement;
+                
+                if ( target.parentNode.nodeName == 'A' ) {
+                    let $el = target.parentNode.childNodes;
+                    
+                    tempEl['item'] = target.parentNode.getAttribute('data-item'),
+                    tempEl['title'] = $el[1].innerText,
+                    tempEl['price'] = $el[2].innerText.split(' 원')[0];
+                }
+                
+                if ( target.nodeName == 'A' ) {
+                    let $el = target.childNodes;
 
-                if ( target.parentNode.nodeName == 'A' || target.nodeName == 'A' ) {
-                    self.popup.open();
+                    tempEl['item'] = target.getAttribute('data-item'),
+                    tempEl['title'] = $el[1].innerText,
+                    tempEl['price'] = $el[2].innerText.split(' 원')[0];
                 }
 
+                popHead.innerText = tempEl['title'];
+                self.popup.open();
             });
 
-            document.getElementById('btnCancel').addEventListener('click', () => {
+            document.getElementById('btnCancel').addEventListener('click', () => self.popup.close());
+            document.getElementById('btnAdd').addEventListener('click', () => {
+                tempEl['type'] = popChk.checked ? 'hot' : 'ice';
+                
+                switch ( tempEl.item ) {
+                    case 'espresso':
+                        setEl =  new Beverage(tempEl.item);
+                        break;
+                    case 'americano':
+                        setEl = tempEl.type == 'hot' ? Water(new Beverage('americano')) : Ice(Water(new Beverage('iceAmericano')));
+                        break;
+                    case 'cafelatte':
+                        setEl = tempEl.type == 'hot' ? Milk(new Beverage('cafelatte')) : Ice(Milk(new Beverage('iceCafelatte')));
+                        break;
+                    case 'banillalatte':
+                        setEl = tempEl.type == 'hot' ? Banilla(Milk(new Beverage('banillalatte'))) : Ice(Banilla(Milk(new Beverage('iceBanillalatte'))));
+                        break;
+                    case 'caramellatte':
+                        setEl = tempEl.type == 'hot' ? Caramel(Milk(new Beverage('caramellatte'))) : Ice(Caramel(Milk(new Beverage('iceCaramellatte'))));
+                        break;
+                    case 'cafemoca':
+                        setEl = tempEl.type == 'hot' ? Mocha(Milk(new Beverage('cafemocha'))) : Ice(Mocha(Milk(new Beverage('iceCafemocha'))));
+                        break;
+                    default:
+                        break;
+                }
+
+                orderArr.push(setEl);
                 self.popup.close();
+                tempEl = [];
+                setEl = [];
             });
 
 
